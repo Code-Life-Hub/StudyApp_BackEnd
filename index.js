063cc0f49e4a97f4d3a7058303796ea5017a7bfb
Unknown User <david.wright.developer@gmail.com>
2024-11-10 22:33:10 -0600

    Successfully Connected dbeaver to cloud DB. Up next is builder backend routing for calls made to DB from the frontEnd.

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 require("dotenv").config();
 const mysql = require("mysql2");
-const fs = require("fs").promises; // Use promises for async/await
-const path = require("path");
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -22,31 +20,3 @@ connection.connect((err) => {
   }
   console.log("Connected to DigitalOcean MySQL database");
 });
-
-// Function to import the SQL file
-async function importSQLFile(filePath) {
-  try {
-    const data = await fs.readFile(filePath, "utf8");
-
-    await new Promise((resolve, reject) => {
-      connection.query(data, (queryErr, results) => {
-        if (queryErr) {
-          console.error("Error executing SQL commands:", queryErr);
-          reject(queryErr);
-        } else {
-          console.log("Database import successful:", results);
-          resolve();
-        }
-      });
-    });
-  } catch (err) {
-    console.error("Error reading or executing SQL file:", err);
-  } finally {
-    // Close the connection when done
-    connection.end();
-  }
-}
-
-// Call the function to import the SQL file
-const sqlFilePath = path.join(__dirname, "studyapp_backup.sql");
-importSQLFile(sqlFilePath);
