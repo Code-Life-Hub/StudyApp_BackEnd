84983c579179eb90ee039925b1fb2ca5072073d9
Unknown User <david.wright.developer@gmail.com>
2024-11-10 20:53:00 -0600

    server work in progress.

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-require('dotenv').config();
-const mysql = require('mysql2');
+require("dotenv").config();
+const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,    
-    user: process.env.DB_USER,    
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    port: 25060,
-    ssl: {
-        rejectUnauthorized: true
-    }
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: 25060,
+  ssl: {
+    rejectUnauthorized: false, // This does not allow self-seigned certs.
+  },
 });
 
 // Connect to the database
 connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to MySQL:', err);
-        return;
-    }
-    console.log('Connected to DigitalOcean MySQL database');
+  if (err) {
+    console.error("Error connecting to MySQL:", err);
+    return;
+  }
+  console.log("Connected to DigitalOcean MySQL database");
 });
